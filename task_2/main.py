ab263bff30d6251569d91be9b6fb6c4a3248c388
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2024-05-09 15:51:34 -0400

    update task_2

--- task_2/main.py
+++ task_2/main.py
@@ -6,11 +6,11 @@ from pymongo.server_api import ServerApi
 
 uri = f"mongodb://localhost:27017"
 
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri)
 
 db = client.cats_mongo
 
-parser = argparse.ArgumentParser(description="Add new cat")
+parser = argparse.ArgumentParser(description="Manage cats database")
 parser.add_argument("--action", help="[create, read, update, delete]")
 parser.add_argument("--id", help="ID of the cat")
 parser.add_argument("--name", help="Name of the cat")
@@ -24,10 +24,6 @@ name = args["name"]
 age = args["age"]
 features = args["features"]
 
-def read():
-    cats = list(db.cats.find({}))
-    return cats
-
 def create(name, age, features):
     return db.cats.insert_one({
         "name": name, 
@@ -35,25 +31,63 @@ def create(name, age, features):
         "features": features
         })
 
-def update(pk, name, age, features):
-    return db.cats.update_one({"_id": ObjectId(pk)}, {"$set": {"name": name, "age": age, "features": features}})
+def read_all():
+    """Read all cats from the collection."""
+    return list(db.cats.find({}))
 
-def delete(pk):
-    return db.cats.delete_one({"_id": ObjectId(pk)})
+def read_by_name(cat_name):
+    """Read information about a cat by its name."""
+    return db.cats.find_one({"name": cat_name})
+
+def update_age_by_name(cat_name, new_age):
+    """ Update the age of a cat by its name. """
+    return db.cats.update_one({"name": cat_name}, {"$set": {"age": new_age}})
+
+def add_feature_by_name(cat_name, new_features):
+    """Add a new feature to the list of features of a cat by its name."""
+    return db.cats.update_one({"name": cat_name}, {"$push": {"features": {"$each": new_features}}})
+
+def delete_by_name(cat_name):
+    """Delete a cat by its name."""
+    return db.cats.delete_one({"name": cat_name})
+
+def delete_all():
+    """Delete all cats from the collection."""
+    return db.cats.delete_many({})
 
 if __name__ == "__main__":
     match action:
         case "create":
             response = create(name, age, features)
             print(response.inserted_id)
+
         case "read":
-            [print(cat) for cat in read()]
+            if name:
+                cat_info = read_by_name(name)
+                if cat_info:
+                    print(cat_info)
+                else:
+                    print("Cat not found.")
+            else:
+                [print(cat) for cat in read_all()]
+
         case "update":
-            response = update(pk, name, age, features)
-            print(response.modified_count)
+            if name and age:
+                response = update_age_by_name(name, age)
+                print(response.modified_count)
+            elif name and features:
+                response = add_feature_by_name(name, features)
+                print(response.modified_count)
+            else:
+                print("Please provide name along with either age or features to update.")
+                
         case "delete":
-            response = delete(pk)
-            print(response.deleted_count)
+            if name:
+                response = delete_by_name(name)
+                print(response.deleted_count)
+            else:
+                response = delete_all()
+                print(response.deleted_count)
         case _:
             print("Wrong action")
 
