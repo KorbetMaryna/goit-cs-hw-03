4cd4e15d01903a718c915293728fa1e83dbf0b77
Maryna-Korbet <korbet.maryna.developer@gmail.com>
2024-05-08 20:55:35 -0400

    update task_2

--- task_2/main.py
+++ task_2/main.py
@@ -3,44 +3,43 @@ from bson.objectid import ObjectId
 
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-
-uri = f"mongodb+srv://{config['USER_MONGODB']}:{config['PASSWORD_MONGODB']}@cats.hniute1.mongodb.net/?retryWrites=true&w=majority"
+uri = f"mongodb://localhost:27017"
 
 client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client.cats_mongo
 
 parser = argparse.ArgumentParser(description="Add new cat")
 parser.add_argument("--action", help="[create, read, update, delete]")
 parser.add_argument("--id", help="ID of the cat")
 parser.add_argument("--name", help="Name of the cat")
 parser.add_argument("--age", help="Age of the cat")
-parser.add_argument("--feature", help="Features of the cat", nargs="+")
+parser.add_argument("--features", help="Features of the cat", nargs='+')
 
 args = vars(parser.parse_args())
 action = args["action"]
 pk = args["id"]
 name = args["name"]
 age = args["age"]
-features = args["feature"]
+features = args["features"]
 
 def read():
-    cats = list(client.cats.cats.find({}))
+    cats = list(db.cats.find({}))
     return cats
 
-def create (name, age, features):
-    return client.cats.cats.insert_one({
+def create(name, age, features):
+    return db.cats.insert_one({
         "name": name, 
         "age": age, 
         "features": features
         })
 
-def update (pk, name, age, features):
-    return client.cats.cats.update_one({"_id": ObjectId(pk)}, {"$set": {"name": name, "age": age, "features": features}})
+def update(pk, name, age, features):
+    return db.cats.update_one({"_id": ObjectId(pk)}, {"$set": {"name": name, "age": age, "features": features}})
 
-def delete (pk):
-    return client.cats.cats.delete_one({"_id": ObjectId(pk)})
+def delete(pk):
+    return db.cats.delete_one({"_id": ObjectId(pk)})
 
 if __name__ == "__main__":
     match action:
@@ -48,7 +47,7 @@ if __name__ == "__main__":
             response = create(name, age, features)
             print(response.inserted_id)
         case "read":
-            [print(cat) for cat in (read())]
+            [print(cat) for cat in read()]
         case "update":
             response = update(pk, name, age, features)
             print(response.modified_count)
